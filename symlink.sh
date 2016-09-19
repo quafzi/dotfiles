4d70f07bc90699879da72893d6250db93fec8f8b
Thomas Birke <quafzi@netextreme.de>
2016-09-19 15:05:30 +0200

    Improve detection of existing symlinks

--- symlink.sh
+++ symlink.sh
@@ -11,7 +11,7 @@ fi
 dotfilesPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # VIM
-if [ ! -e $HOME/.vimrc ]; then
+if [ ! -L $HOME/.vimrc ]; then
   ln -s $dotfilesPath/vim/vimrc ~/.vimrc
 else
   if [ `readlink $HOME/.vimrc` != "$dotfilesPath/vim/vimrc" ]; then
@@ -20,7 +20,7 @@ else
 fi
 
 # NEOVIM
-if [ ! -e $configPath/nvim ]; then
+if [ ! -L $configPath/nvim ]; then
   ln -s $dotfilesPath/vim $configPath/nvim
 else
   if [ "`readlink $configPath/nvim`" != "$dotfilesPath/vim" ]; then
@@ -29,7 +29,7 @@ else
 fi
 
 # AWESOME
-if [ ! -e $configPath/awesome ]; then
+if [ ! -L $configPath/awesome ]; then
   ln -s $dotfilesPath/awesome $configPath/awesome
 else
   if [ "`readlink $configPath/awesome`" != "$dotfilesPath/awesome" ]; then
@@ -38,7 +38,7 @@ else
 fi
 
 # I3
-if [ ! -e $configPath/i3 ]; then
+if [ ! -L $configPath/i3 ]; then
   ln -s $dotfilesPath/i3 $configPath/i3
 else
   if [ "`readlink $configPath/i3`" != "$dotfilesPath/i3" ]; then
