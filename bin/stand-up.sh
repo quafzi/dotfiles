acb05b2fa6631033aa88b875c2e2bc3b732157e8
Thomas Birke <quafzi@netextreme.de>
2018-04-26 12:41:47 +0200

    Add reminder script to switch between sitting and standing work position

---
+++ bin/stand-up.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#
+# RUN THIS VIA CRON to get a notification to stand up and take care for your body 😏
+#
+# e.g.
+# 10 * * * * ~/dotfiles/offlineimap/notify.sh
+# 
+userId=`id -u`
+export DISPLAY=:0; export XAUTHORITY=/run/user/$userId/gdm/Xauthority;
+
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userId/bus"
+export DBUS_SESSION_BUS_ADDRESS
+
+notify-send "Aufstehen! 😏"
